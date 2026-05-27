a727a08db22035646a85114c0c399ec17ca08530
JassonTen <extremoandy18@gmail.com>
2026-05-26 19:47:08 -0500

    fix: solucionar el ApplicationContext de Cucumber para Jenkins

--- src/test/java/pe/edu/upeu/api_orders/CucumberSpringConfiguration.java
+++ src/test/java/pe/edu/upeu/api_orders/CucumberSpringConfiguration.java
@@ -1,10 +1,11 @@
 package pe.edu.upeu.api_orders;
 
 import org.springframework.boot.test.context.SpringBootTest;
-
 import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfiguration {
+    // Al quitarle (classes = ApiOrdersApplication.class), obligamos a Spring 
+    // a buscar de forma automática la raíz @SpringBootApplication sin marearse.
 }
